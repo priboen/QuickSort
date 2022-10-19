31caefbba494a4f15ede5471e3945328daaf63b2
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2022-10-19 16:43:26 +0700

    make varriable n (number of element in array)

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ namespace QuickSort
         private int[] arr = new int[20];
         private int cmp_count = 0;
         private int mov_count = 0;
+
+        private int n;
         static void Main(string[] args)
         {
         }
