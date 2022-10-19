dc4256a19a71713a14c655b7ca0edadf81ac20b8
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2022-10-19 16:55:07 +0700

    adding function in for loop method

--- Program.cs
+++ Program.cs
@@ -32,7 +32,9 @@ namespace QuickSort
 
             for (int i = 0;i < n; i++)
             {
-
+                Console.Write("<" + (i +1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
             }
         }
         static void Main(string[] args)
