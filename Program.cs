ff22a262106d60dee7abc1dc027986dc2271e936
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2022-10-19 16:48:44 +0700

    add if else in while true loop

--- Program.cs
+++ Program.cs
@@ -21,6 +21,10 @@ namespace QuickSort
                 Console.Write("Enter the number of elements in the array : ");
                 string s = Console.ReadLine();
                 n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 elements \n");
             }
         }
         static void Main(string[] args)
