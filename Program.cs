e668f38e603a69cb1d6c8ad2b43b1ffa4c6a200e
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2022-10-19 16:42:21 +0700

    make cmp_count varriable

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace QuickSort
     class Program
     {
         private int[] arr = new int[20];
+        private int cmp_count = 0;
         static void Main(string[] args)
         {
         }
